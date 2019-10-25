eed5d5d456ecb2c37ae2d18a7c37fe065966143e
bingbin.dong <bingbin.dong@bestwehotel.com>
2019-10-25 16:13:08 +0800

    添加测速工具

--- app/src/main/java/com/dongbingbin/nativeutils/MainActivity.kt
+++ app/src/main/java/com/dongbingbin/nativeutils/MainActivity.kt
@@ -23,18 +23,33 @@ class MainActivity : AppCompatActivity(),CoroutineScope {
 //        var intent = Intent(this,MainActivity.class);
 //        startActivity(intent);
         job = Job()
-        launch {
-            println("dongbingbin 1:"+Thread.currentThread().name)
-            val ioData = async(Dispatchers.IO) { // <- launch scope 的扩展函数，指定了 IO dispatcher，所以在 IO 线程运行
-                // 在这里执行阻塞的 I/O 耗时操作
-                println("dongbingbin 2:"+Thread.currentThread().name)
-            }
+//        var begin2=System.currentTimeMillis();
+//        for (i in 1..100) {
+//            launch {
+//                println("dongbingbin 1:" + Thread.currentThread().name)
+//                val ioData = async(Dispatchers.IO) {
+//                    // <- launch scope 的扩展函数，指定了 IO dispatcher，所以在 IO 线程运行
+//                    // 在这里执行阻塞的 I/O 耗时操作
+//                    println("dongbingbin 2:" + Thread.currentThread().name)
+//                }
+//
+//                // 和上面的并非 I/O 同时执行的其他操作
+//                val data = ioData.await() // 等待阻塞 I/O 操作的返回结果
+//                println("dongbingbin 3:" + Thread.currentThread().name)
+//                //draw(data) // 在 UI 线程显示执行的结果
+//            }
+//        }
 
-            // 和上面的并非 I/O 同时执行的其他操作
-            val data = ioData.await() // 等待阻塞 I/O 操作的返回结果
-            println("dongbingbin 3:"+Thread.currentThread().name)
-            //draw(data) // 在 UI 线程显示执行的结果
-        }
+       // println("dongbingbin 1:执行完毕 耗时:"+((System.currentTimeMillis()-begin2)))
+
+//        var begin1 = System.currentTimeMillis()
+//        for(i in 1..10000){
+//            Thread{
+//                println("dongbingbin 4:" + Thread.currentThread().name)
+//            }.start();
+//        }
+//
+//        println("dongbingbin 4:执行完毕 耗时:"+((System.currentTimeMillis()-begin1)))
     }
 
     override val coroutineContext: CoroutineContext
