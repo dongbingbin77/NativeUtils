3ab289fd25cbe17b2c2aadcec3c4d37c1510657f
dongbingbin77 <56656174@qq.com>
2020-04-29 00:46:05 +0800

    添加自定义plugin

--- testplugin/src/main/java/com/yjy/testplugin/TestPlugin.java
+++ testplugin/src/main/java/com/yjy/testplugin/TestPlugin.java
@@ -1,4 +1,12 @@
 package com.yjy.testplugin;
 
+import com.android.build.gradle.external.cmake.server.Project;
+
+import org.gradle.api.Plugin;
+
 public class TestPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
 }
